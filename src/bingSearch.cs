b6508c6f8a586c5ac8442ab1b78f6582c76e49fa
Maciej Czarkowski <maciej.czarkowski@emergeconsulting.pl>
2021-01-26 23:57:08 +0100

    add source code for custom skills

---
+++ src/bingSearch.cs
@@ -0,0 +1,257 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace SampleSkills
+{
+    /// <summary>
+    /// Sample custom skill that wraps the Bing entity search API to connect it with a 
+    /// AI enrichment pipeline.
+    /// </summary>
+    public static class BingEntitySearch
+    {
+        #region Credentials
+        // IMPORTANT: Make sure to enter your credential and to verify the API endpoint matches yours.
+        static readonly string bingApiEndpoint = "https://api.cognitive.microsoft.com/bing/v7.0/entities/";
+        static readonly string key = "<enter your api key here>";  
+        #endregion
+
+        #region Class used to deserialize the request
+        private class InputRecord
+        {
+            public class InputRecordData
+            {
+                public string Name { get; set; }
+            }
+
+            public string RecordId { get; set; }
+            public InputRecordData Data { get; set; }
+        }
+
+        private class WebApiRequest
+        {
+            public List<InputRecord> Values { get; set; }
+        }
+        #endregion
+
+        #region Classes used to serialize the response
+
+        private class OutputRecord
+        {
+            public class OutputRecordData
+            {
+                public string Name { get; set; } = "";
+                public string Description { get; set; } = "";
+                public string Source { get; set; } = "";
+                public string SourceUrl { get; set; } = "";
+                public string LicenseAttribution { get; set; } = "";
+                public string LicenseUrl { get; set; } = "";
+            }
+
+            public class OutputRecordMessage
+            {
+                public string Message { get; set; }
+            }
+
+            public string RecordId { get; set; }
+            public OutputRecordData Data { get; set; }
+            public List<OutputRecordMessage> Errors { get; set; }
+            public List<OutputRecordMessage> Warnings { get; set; }
+        }
+
+        private class WebApiResponse
+        {
+            public List<OutputRecord> Values { get; set; }
+        }
+        #endregion
+
+        #region Classes used to interact with the Bing API
+        private class BingResponse
+        {
+            public BingEntities Entities { get; set; }
+        }
+        private class BingEntities
+        {
+            public BingEntity[] Value { get; set; }
+        }
+
+        private class BingEntity
+        {
+            public class EntityPresentationinfo
+            {
+                public string[] EntityTypeHints { get; set; }
+            }
+
+            public class License
+            {
+                public string Url { get; set; }
+            }
+
+            public class ContractualRule
+            {
+                public string _type { get; set; }
+                public License License { get; set; }
+                public string LicenseNotice { get; set; }
+                public string Text { get; set; }
+                public string Url { get; set; }
+            }
+
+            public ContractualRule[] ContractualRules { get; set; }
+            public string Description { get; set; }
+            public string Name { get; set; }
+            public EntityPresentationinfo EntityPresentationInfo { get; set; }
+        }
+        #endregion
+
+        #region The Azure Function definition
+
+        [FunctionName("EntitySearch")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Entity Search function: C# HTTP trigger function processed a request.");
+
+            var response = new WebApiResponse
+            {
+                Values = new List<OutputRecord>()
+            };
+
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            var data = JsonConvert.DeserializeObject<WebApiRequest>(requestBody);
+
+            // Do some schema validation
+            if (data == null)
+            {
+                return new BadRequestObjectResult("The request schema does not match expected schema.");
+            }
+            if (data.Values == null)
+            {
+                return new BadRequestObjectResult("The request schema does not match expected schema. Could not find values array.");
+            }
+
+            // Calculate the response for each value.
+            foreach (var record in data.Values)
+            {
+                if (record == null || record.RecordId == null) continue;
+
+                OutputRecord responseRecord = new OutputRecord
+                {
+                    RecordId = record.RecordId
+                };
+
+                try
+                {
+                    responseRecord.Data = GetEntityMetadata(record.Data.Name).Result;
+                }
+                catch (Exception e)
+                {
+                    // Something bad happened, log the issue.
+                    var error = new OutputRecord.OutputRecordMessage
+                    {
+                        Message = e.Message
+                    };
+
+                    responseRecord.Errors = new List<OutputRecord.OutputRecordMessage>
+                    {
+                        error
+                    };
+                }
+                finally
+                {
+                    response.Values.Add(responseRecord);
+                }
+            }
+
+            return (ActionResult)new OkObjectResult(response);
+        }
+
+        #endregion
+
+        #region Methods to call the Bing API
+        /// <summary>
+        /// Gets metadata for a particular entity based on its name using Bing Entity Search
+        /// </summary>
+        /// <param name="entityName">The name of the entity to extract data for.</param>
+        /// <returns>Asynchronous task that returns entity data. </returns>
+        private async static Task<OutputRecord.OutputRecordData> GetEntityMetadata(string entityName)
+        {
+            var uri = bingApiEndpoint + "?q=" + entityName + "&mkt=en-us&count=10&offset=0&safesearch=Moderate";
+            var result = new OutputRecord.OutputRecordData();
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(uri)
+            })
+            {
+                request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+
+                HttpResponseMessage response = await client.SendAsync(request);
+                string responseBody = await response?.Content?.ReadAsStringAsync();
+
+                BingResponse bingResult = JsonConvert.DeserializeObject<BingResponse>(responseBody);
+                if (bingResult != null)
+                {
+                    // In addition to the list of entities that could match the name, for simplicity let's return information
+                    // for the top match as additional metadata at the root object.
+                    return AddTopEntityMetadata(bingResult.Entities?.Value);
+                }
+            }
+
+            return result;
+        }
+
+        private static OutputRecord.OutputRecordData AddTopEntityMetadata(BingEntity[] entities)
+        {
+            if (entities != null)
+            {
+                foreach (BingEntity entity in entities.Where(
+                    entity => entity?.EntityPresentationInfo?.EntityTypeHints != null
+                        && (entity.EntityPresentationInfo.EntityTypeHints[0] == "Person"
+                            || entity.EntityPresentationInfo.EntityTypeHints[0] == "Organization"
+                            || entity.EntityPresentationInfo.EntityTypeHints[0] == "Location")
+                        && !String.IsNullOrEmpty(entity.Description)))
+                {
+                    var rootObject = new OutputRecord.OutputRecordData
+                    {
+                        Description = entity.Description,
+                        Name = entity.Name
+                    };
+
+                    if (entity.ContractualRules != null)
+                    {
+                        foreach (var rule in entity.ContractualRules)
+                        {
+                            switch (rule._type)
+                            {
+                                case "ContractualRules/LicenseAttribution":
+                                    rootObject.LicenseAttribution = rule.LicenseNotice;
+                                    rootObject.LicenseUrl = rule.License.Url;
+                                    break;
+                                case "ContractualRules/LinkAttribution":
+                                    rootObject.Source = rule.Text;
+                                    rootObject.SourceUrl = rule.Url;
+                                    break;
+                            }
+                        }
+                    }
+
+                    return rootObject;
+                }
+            }
+
+            return new OutputRecord.OutputRecordData();
+        }
+        #endregion
+    }
+}
---
+++ src/concatenator.py
@@ -0,0 +1,75 @@
+import logging
+import azure.functions as func
+import json
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    try:
+        body = json.dumps(req.get_json())
+    except ValueError:
+        return func.HttpResponse(
+             "Invalid body",
+             status_code=400
+        )
+    
+    if body:
+        result = compose_response(body)
+        return func.HttpResponse(result, mimetype="application/json")
+    else:
+        return func.HttpResponse(
+             "Invalid body",
+             status_code=400
+        )
+
+
+def compose_response(json_data):
+    values = json.loads(json_data)['values']
+    
+    # Prepare the Output before the loop
+    results = {}
+    results["values"] = []
+    
+    for value in values:
+        output_record = transform_value(value)
+        if output_record != None:
+            results["values"].append(output_record)
+    return json.dumps(results, ensure_ascii=False)
+
+## Perform an operation on a record
+def transform_value(value):
+    try:
+        recordId = value['recordId']
+    except AssertionError  as error:
+        return None
+
+    # Validate the inputs
+    try:         
+        assert ('data' in value), "'data' field is required."
+        data = value['data']        
+        assert ('text1' in data), "'text1' field is required in 'data' object."
+        assert ('text2' in data), "'text2' field is required in 'data' object."
+    except AssertionError  as error:
+        return (
+            {
+            "recordId": recordId,
+            "errors": [ { "message": "Error:" + error.args[0] }   ]       
+            })
+
+    try:                
+        concatenated_string = value['data']['text1'] + " " + value['data']['text2']  
+        # Here you could do something more interesting with the inputs
+
+    except:
+        return (
+            {
+            "recordId": recordId,
+            "errors": [ { "message": "Could not complete operation for record." }   ]       
+            })
+
+    return ({
+            "recordId": recordId,
+            "data": {
+                "text": concatenated_string
+                    }
+            })
